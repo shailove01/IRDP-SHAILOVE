2953849953a7fa5bf64d75f1b846660c6fde2ecf
shailove01 <gaminghustleii@gmail.com>
2026-07-05 23:54:01 +0530

    Some optimal code changes done .

---
+++ Chapter8-ps(OOPs)/OOPs.py
@@ -0,0 +1,23 @@
+
+
+## OOPs :-
+'''
+ Solving a problem by creationg an object is one of the most popular appraches
+in programming. This is called Object Oriented Programming. 
+
+This concept focuses on using reusable code (DRY Principle).
+
+
+CLASS:- 
+
+A class is a blueprint for creating object.
+
+
+
+OBJECT:- 
+
+An object is an instantiation of a class.
+When a class is defined , a template(info) is defined.
+Memory is allocated only after object instantiation. 
+   '''
+
